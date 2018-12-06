8d442f7256249b02fa8a02951c37370a6c4f52f4
nelsonibarra10 <nelsonaibarra@hotmail.com>
2018-12-06 19:50:11 -0300

    primary
    1er

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {helloworld} from './primercomponente/primer.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    helloworld,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
