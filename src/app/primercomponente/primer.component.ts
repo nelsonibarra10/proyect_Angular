674d0f65fad26e974093b83ea39f64756a084d15
nelsonibarra10 <nelsonaibarra@hotmail.com>
2018-12-09 14:23:39 -0300

    cambios

--- src/app/primercomponente/primer.component.ts
+++ src/app/primercomponente/primer.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'hello-world',
     templateUrl: './primer.component.html',
-    styleUrls: ['./primer.component.css']
+    styleUrls: ['./primer.component.css'],
 })
 export class helloworld {
 
